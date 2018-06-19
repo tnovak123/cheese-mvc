549227e53c623a71691b14c935e960abb3502739
tnovak123 <timothy.m.novak@gmail.com>
2018-06-19 18:19:48 -0500

    Program up to Enumeration.

---
+++ src/main/java/org/launchcode/classes/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,69 @@
+package org.launchcode.classes.cheesemvc.controllers;
+
+import org.launchcode.classes.cheesemvc.models.Cheese;
+import org.launchcode.classes.cheesemvc.models.CheeseData;
+import org.launchcode.classes.cheesemvc.models.CheeseType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+
+
+@Controller
+@RequestMapping("cheese")
+public class CheeseController {
+
+    // Request path: /cheese
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("cheeses", CheeseData.getAll());
+        model.addAttribute("title", "My Cheeses");
+
+        return "cheese/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        model.addAttribute(new Cheese());
+        model.addAttribute("cheeseTypes", CheeseType.values());
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(Model model, @ModelAttribute @Valid Cheese newCheese, Errors errors) {
+
+        if (errors.hasErrors()){
+            model.addAttribute("title", "Add Cheese");
+            //model.addAttribute(new Cheese());
+            return "cheese/add";
+        }
+
+        CheeseData.add(newCheese);
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model) {
+        model.addAttribute("cheeses", CheeseData.getAll());
+        model.addAttribute("title", "Remove Cheese");
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds) {
+
+        for (int cheeseId : cheeseIds) {
+            CheeseData.remove(cheeseId);
+        }
+
+        return "redirect:";
+    }
+
+}
---
+++ src/main/java/org/launchcode/classes/cheesemvc/models/Cheese.java
@@ -0,0 +1,63 @@
+package org.launchcode.classes.cheesemvc.models;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Cheese {
+
+    @NotNull
+    @Size(min=3, max=15, message="Name must be between 3 and 15 characters!")
+    private String name;
+
+    @NotNull
+    @Size(min=1, message="Description must not be empty!")
+    private String description;
+
+    private CheeseType type;
+
+    private int cheeseId;
+    private static int nextId = 1;
+
+    public Cheese(String name, String description) {
+        this();
+        this.name = name;
+        this.description = description;
+    }
+
+    public Cheese() {
+        cheeseId = nextId;
+        nextId++;
+    }
+
+    public int getCheeseId() {
+        return cheeseId;
+    }
+
+    public void setCheeseId(int cheeseId) {
+        this.cheeseId = cheeseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public CheeseType getType() {
+        return type;
+    }
+
+    public void setType(CheeseType type) {
+        this.type = type;
+    }
+}
