ee704b7f9f1d45154cf9110ac81dac88ec5a07ea
tnovak123 <timothy.m.novak@gmail.com>
2018-07-31 21:12:31 -0500

    Added Cascade delete for the chesses.

--- src/main/java/org/launchcode/classes/cheesemvc/models/Menu.java
+++ src/main/java/org/launchcode/classes/cheesemvc/models/Menu.java
@@ -1,9 +1,6 @@
 package org.launchcode.classes.cheesemvc.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
@@ -19,7 +16,7 @@ public class Menu {
     @Size(min = 3, max = 15)
     private String name;
 
-    @ManyToMany
+    @ManyToMany(cascade = CascadeType.PERSIST)
     private List<Cheese> cheeses;
 
     public Menu() {  }
